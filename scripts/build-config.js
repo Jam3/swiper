9aa6195563438b1eb675ab30c52f451d1c610ba0
Iran Reyes <iran.reyes@jam3.com>
2019-10-07 10:09:11 -0400

    dist: create custom build

--- scripts/build-config.js
+++ scripts/build-config.js
@@ -1,29 +1,9 @@
 module.exports = {
-  components: [
-    'virtual',
-    'keyboard',
-    'mousewheel',
-    'navigation',
-    'pagination',
-    'scrollbar',
-    'parallax',
-    'zoom',
-    'lazy',
-    'controller',
-    'a11y',
-    'history',
-    'hash-navigation',
-    'autoplay',
-    'effect-fade',
-    'effect-cube',
-    'effect-flip',
-    'effect-coverflow',
-    'thumbs',
-  ],
-  target: 'universal',
+  components: ['navigation', 'pagination'],
+  target: 'browser',
   themeColor: '#007aff',
   colors: {
     white: '#ffffff',
-    black: '#000000',
-  },
+    black: '#000000'
+  }
 };
